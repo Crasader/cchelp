02057b6c0f1dd7c3ddda462d5d5262f641376953
Saraid <saraid@vinova.sg>
2015-05-05 14:17:17 +0700

    + hash map with custom hash

--- jsoncpp/hash_map.hpp
+++ jsoncpp/hash_map.hpp
@@ -71,6 +71,72 @@ namespace Json {
             
             return jVal;
         }
+        
+        template <typename V, typename H>
+        inline bool deserialize(const Json::Value &j, hmap<string, V, H> &v)
+        {
+            v.clear();
+            if (j.isObject())
+            {
+                for (ValueIterator it = j.begin(); it != j.end(); ++it)
+                {
+                    string key = it.key().asString();
+                    V val;
+                    Json::type::deserialize((*it), val);
+                    v[key] = val;
+                }
+                return true;
+            }
+            
+            return false;
+        }
+        
+        
+        template <typename V, typename H>
+        inline Json::Value serialize(const hmap<string, V, H> &v)
+        {
+            Value jVal;
+            for (auto it = v.cbegin(); it != v.cend(); ++it)
+            {
+                jVal[it->first] = Json::type::serialize(it->second);
+            }
+            
+            return jVal;
+        }
+        
+        template <typename K, typename V, typename H>
+        inline bool deserialize(const Json::Value &j, hmap<K, V, H> &v)
+        {
+            v.clear();
+            if (j.isObject())
+            {
+                for (Json::ValueIterator it = j.begin(); it != j.end(); ++it)
+                {
+                    K key;
+                    Json::type::strDeserialize(it.key().asString(), key);
+                    V val;
+                    Json::type::deserialize((*it), val);
+                    v[key] = val;
+                }
+                return true;
+            }
+            
+            return false;
+        }
+        
+        
+        template <typename K, typename V, typename H>
+        inline Json::Value serialize(const hmap<K, V, H> &v)
+        {
+            Value jVal;
+            for (auto it = v.cbegin(); it != v.cend(); ++it)
+            {
+                std::string key = Json::type::strSerialize(it->first);
+                jVal[key] = Json::type::serialize(it->second);
+            }
+            
+            return jVal;
+        }
     }
 }  // namespace Json
 
