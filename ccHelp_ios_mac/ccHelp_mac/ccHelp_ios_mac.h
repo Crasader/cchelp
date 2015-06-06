8c9e4f763f5e7ee51e3b95460be4e24c2eb78a76
Saraid <saraid@vinova.sg>
2015-06-06 11:51:38 +0700

    + mac & ios project

---
+++ ccHelp_ios_mac/ccHelp_mac/ccHelp_ios_mac.h
@@ -0,0 +1,23 @@
+/*
+ *  ccHelp_ios_mac.h
+ *  ccHelp_ios_mac
+ *
+ *  Created by Vinova on 6/6/15.
+ *  Copyright (c) 2015 Less. All rights reserved.
+ *
+ */
+
+#ifndef ccHelp_ios_mac_
+#define ccHelp_ios_mac_
+
+/* The classes below are exported */
+#pragma GCC visibility push(default)
+
+class ccHelp_ios_mac
+{
+	public:
+		void HelloWorld(const char *);
+};
+
+#pragma GCC visibility pop
+#endif
---
+++ ccHelp_ios_mac/ccHelp_mac/ccHelp_ios_macPriv.h
@@ -0,0 +1,19 @@
+/*
+ *  ccHelp_ios_macPriv.h
+ *  ccHelp_ios_mac
+ *
+ *  Created by Vinova on 6/6/15.
+ *  Copyright (c) 2015 Less. All rights reserved.
+ *
+ */
+
+/* The classes below are not exported */
+#pragma GCC visibility push(hidden)
+
+class ccHelp_ios_macPriv
+{
+	public:
+		void HelloWorldPriv(const char *);
+};
+
+#pragma GCC visibility pop
