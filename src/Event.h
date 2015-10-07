34141e3720882c2306573a4e6df79282c01bd181
Saraid <saraid@vinova.sg>
2015-10-07 22:11:39 +0700

    detachable event

--- src/Event.h
+++ src/Event.h
@@ -1,22 +1,29 @@
 #pragma once
 #include <hash_container/hmap.h>
+//#include <map>
 #include <list>
 #include <functional>
+#include <string>
+#include <assert.h>
 
 namespace ccHelp
 {
+//#define hmap std::map
+    
     template<typename T1 = void, typename T2 = void, typename T3 = void, typename T4 = void, typename T5 = void>
     struct Event;
     
+#pragma mark 0
     template<>
     struct Event<void()>
     {
-        hmap<std::string, std::function<void()>*> namedFuncs;
-        std::list<std::function<void()>> funcs;
+        typedef std::function<void()> Func;
+        hmap<std::string, Func*> namedFuncs;
+        std::list<Func> funcs;
         
         Event<void()>() {}
         
-        Event<void()>& operator-=(std::function<void()> f)
+        Event<void()>& operator-=(Func f)
         {
             auto ite = funcs.begin();
             while (ite != funcs.end())
@@ -73,7 +80,7 @@ namespace ccHelp
         
         void operator()()
         {
-            std::list<std::function<void()>> _copy(funcs);
+            std::list<Func> _copy(funcs);
             for (auto &f : _copy)
             {
                 if (f)
@@ -81,12 +88,31 @@ namespace ccHelp
             }
         }
         
-        void add(std::function<void()> f, const std::string &name)
+        void add(Func f, const std::string &name)
         {
             assert(namedFuncs.find(name) == namedFuncs.end());
             
             funcs.push_back(f);
             namedFuncs[name] = &(funcs.back());
+        }
+        
+        void remove(const std::string &name)
+        {
+            auto it = namedFuncs.find(name);
+            if (it == namedFuncs.end())
+                return;
+            
+            auto *f = it->second;
+            namedFuncs.erase(it);
+            
+            for (auto it2 = funcs.begin(); it2 != funcs.end(); ++it2)
+            {
+                if ((&(*it2)) == f)
+                {
+                    funcs.erase(it2);
+                    return;
+                }
+            }
         }
         
     private:
@@ -95,14 +121,18 @@ namespace ccHelp
         Event<void()>& operator=(const Event<void()> &e) {return *this;}
     };
     
+    
+#pragma mark 1
     template<typename T1>
     struct Event<void(T1)>
     {
-        std::list<std::function<void(T1)>> funcs;
+        typedef std::function<void(T1)> Func;
+        hmap<std::string, Func*> namedFuncs;
+        std::list<Func> funcs;
         
         Event<void(T1)>() {}
         
-        Event<void(T1)>& operator-=(std::function<void(T1)> f)
+        Event<void(T1)>& operator-=(Func f)
         {
             auto ite = funcs.begin();
             while (ite != funcs.end())
@@ -143,6 +173,7 @@ namespace ccHelp
         void clear()
         {
             funcs.clear();
+            namedFuncs.clear();
         }
         
         operator bool()
@@ -158,11 +189,38 @@ namespace ccHelp
         
         void operator()(T1 p1)
         {
-            std::list<std::function<void(T1)>> _copy(funcs);
+            std::list<Func> _copy(funcs);
             for (auto &f : _copy)
             {
                 if (f)
                     f(p1);
+            }
+        }
+        
+        void add(Func f, const std::string &name)
+        {
+            assert(namedFuncs.find(name) == namedFuncs.end());
+            
+            funcs.push_back(f);
+            namedFuncs[name] = &(funcs.back());
+        }
+        
+        void remove(const std::string &name)
+        {
+            auto it = namedFuncs.find(name);
+            if (it == namedFuncs.end())
+                return;
+            
+            auto *f = it->second;
+            namedFuncs.erase(it);
+            
+            for (auto it2 = funcs.begin(); it2 != funcs.end(); ++it2)
+            {
+                if ((&(*it2)) == f)
+                {
+                    funcs.erase(it2);
+                    return;
+                }
             }
         }
         
@@ -172,14 +230,17 @@ namespace ccHelp
         Event<void(T1)>& operator=(const Event<void(T1)> &e) {return *this;}
     };
     
+#pragma mark 2
     template<typename T1, typename T2>
     struct Event<void(T1, T2)>
     {
-        std::list<std::function<void(T1, T2)>> funcs;
+        typedef std::function<void(T1, T2)> Func;
+        hmap<std::string, Func*> namedFuncs;
+        std::list<Func> funcs;
         
         Event<void(T1, T2)>() {}
         
-        Event<void(T1, T2)>& operator-=(std::function<void(T1, T2)> f)
+        Event<void(T1, T2)>& operator-=(Func f)
         {
             auto ite = funcs.begin();
             while (ite != funcs.end())
@@ -220,6 +281,7 @@ namespace ccHelp
         void clear()
         {
             funcs.clear();
+            namedFuncs.clear();
         }
         
         operator bool()
@@ -235,11 +297,38 @@ namespace ccHelp
         
         void operator()(T1 p1, T2 p2)
         {
-            std::list<std::function<void(T1, T2)>> _copy(funcs);
+            std::list<Func> _copy(funcs);
             for (auto &f : _copy)
             {
                 if (f)
                     f(p1, p2);
+            }
+        }
+        
+        void add(Func f, const std::string &name)
+        {
+            assert(namedFuncs.find(name) == namedFuncs.end());
+            
+            funcs.push_back(f);
+            namedFuncs[name] = &(funcs.back());
+        }
+        
+        void remove(const std::string &name)
+        {
+            auto it = namedFuncs.find(name);
+            if (it == namedFuncs.end())
+                return;
+            
+            auto *f = it->second;
+            namedFuncs.erase(it);
+            
+            for (auto it2 = funcs.begin(); it2 != funcs.end(); ++it2)
+            {
+                if ((&(*it2)) == f)
+                {
+                    funcs.erase(it2);
+                    return;
+                }
             }
         }
         
@@ -249,14 +338,17 @@ namespace ccHelp
         Event<void(T1, T2)>& operator=(const Event<void(T1, T2)> &e) {return *this;}
     };
     
+#pragma mark 3
     template<typename T1, typename T2, typename T3>
     struct Event<void(T1, T2, T3)>
     {
-        std::list<std::function<void(T1, T2, T3)>> funcs;
+        typedef std::function<void(T1, T2, T3)> Func;
+        hmap<std::string, Func*> namedFuncs;
+        std::list<Func> funcs;
         
         Event<void(T1, T2, T3)>() {}
         
-        Event<void(T1, T2, T3)>& operator-=(std::function<void(T1, T2, T3)> f)
+        Event<void(T1, T2, T3)>& operator-=(Func f)
         {
             auto ite = funcs.begin();
             while (ite != funcs.end())
@@ -297,6 +389,7 @@ namespace ccHelp
         void clear()
         {
             funcs.clear();
+            namedFuncs.clear();
         }
         
         operator bool()
@@ -312,11 +405,38 @@ namespace ccHelp
         
         void operator()(T1 p1, T2 p2, T3 p3)
         {
-            std::list<std::function<void(T1, T2, T3)>> _copy(funcs);
+            std::list<Func> _copy(funcs);
             for (auto &f : _copy)
             {
                 if (f)
                     f(p1, p2, p3);
+            }
+        }
+        
+        void add(Func f, const std::string &name)
+        {
+            assert(namedFuncs.find(name) == namedFuncs.end());
+            
+            funcs.push_back(f);
+            namedFuncs[name] = &(funcs.back());
+        }
+        
+        void remove(const std::string &name)
+        {
+            auto it = namedFuncs.find(name);
+            if (it == namedFuncs.end())
+                return;
+            
+            auto *f = it->second;
+            namedFuncs.erase(it);
+            
+            for (auto it2 = funcs.begin(); it2 != funcs.end(); ++it2)
+            {
+                if ((&(*it2)) == f)
+                {
+                    funcs.erase(it2);
+                    return;
+                }
             }
         }
         
@@ -326,14 +446,17 @@ namespace ccHelp
         Event<void(T1, T2, T3)>& operator=(const Event<void(T1, T2, T3)> &e) {return *this;}
     };
     
+#pragma mark 4
     template<typename T1, typename T2, typename T3, typename T4>
     struct Event<void(T1, T2, T3, T4)>
     {
-        std::list<std::function<void(T1, T2, T3, T4)>> funcs;
+        typedef std::function<void(T1, T2, T3, T4)> Func;
+        hmap<std::string, Func*> namedFuncs;
+        std::list<Func> funcs;
         
         Event<void(T1, T2, T3, T4)>() {}
         
-        Event<void(T1, T2, T3, T4)>& operator-=(std::function<void(T1, T2, T3, T4)> f)
+        Event<void(T1, T2, T3, T4)>& operator-=(Func f)
         {
             auto ite = funcs.begin();
             while (ite != funcs.end())
@@ -374,6 +497,7 @@ namespace ccHelp
         void clear()
         {
             funcs.clear();
+            namedFuncs.clear();
         }
         
         operator bool()
@@ -389,11 +513,38 @@ namespace ccHelp
         
         void operator()(T1 p1, T2 p2, T3 p3, T4 p4)
         {
-            std::list<std::function<void(T1, T2, T3, T4)>> _copy(funcs);
+            std::list<Func> _copy(funcs);
             for (auto &f : _copy)
             {
                 if (f)
                     f(p1, p2, p3, p4);
+            }
+        }
+        
+        void add(Func f, const std::string &name)
+        {
+            assert(namedFuncs.find(name) == namedFuncs.end());
+            
+            funcs.push_back(f);
+            namedFuncs[name] = &(funcs.back());
+        }
+        
+        void remove(const std::string &name)
+        {
+            auto it = namedFuncs.find(name);
+            if (it == namedFuncs.end())
+                return;
+            
+            auto *f = it->second;
+            namedFuncs.erase(it);
+            
+            for (auto it2 = funcs.begin(); it2 != funcs.end(); ++it2)
+            {
+                if ((&(*it2)) == f)
+                {
+                    funcs.erase(it2);
+                    return;
+                }
             }
         }
         
@@ -403,14 +554,17 @@ namespace ccHelp
         Event<void(T1, T2, T3, T4)>& operator=(const Event<void(T1, T2, T3, T4)> &e) {return *this;}
     };
     
+#pragma mark 5
     template<typename T1, typename T2, typename T3, typename T4, typename T5>
     struct Event<void(T1, T2, T3, T4, T5)>
     {
-        std::list<std::function<void(T1, T2, T3, T4, T5)>> funcs;
+        typedef std::function<void(T1, T2, T3, T4, T5)> Func;
+        hmap<std::string, Func*> namedFuncs;
+        std::list<Func> funcs;
         
         Event<void(T1, T2, T3, T4, T5)>() {}
         
-        Event<void(T1, T2, T3, T4, T5)>& operator-=(std::function<void(T1, T2, T3, T4, T5)> f)
+        Event<void(T1, T2, T3, T4, T5)>& operator-=(Func f)
         {
             auto ite = funcs.begin();
             while (ite != funcs.end())
@@ -451,6 +605,7 @@ namespace ccHelp
         void clear()
         {
             funcs.clear();
+            namedFuncs.clear();
         }
         
         operator bool()
@@ -466,11 +621,38 @@ namespace ccHelp
         
         void operator()(T1 p1, T2 p2, T3 p3, T4 p4, T5 p5)
         {
-            std::list<std::function<void(T1, T2, T3, T4, T5)>> _copy(funcs);
+            std::list<Func> _copy(funcs);
             for (auto &f : _copy)
             {
                 if (f)
                     f(p1, p2, p3, p4, p5);
+            }
+        }
+        
+        void add(Func f, const std::string &name)
+        {
+            assert(namedFuncs.find(name) == namedFuncs.end());
+            
+            funcs.push_back(f);
+            namedFuncs[name] = &(funcs.back());
+        }
+        
+        void remove(const std::string &name)
+        {
+            auto it = namedFuncs.find(name);
+            if (it == namedFuncs.end())
+                return;
+            
+            auto *f = it->second;
+            namedFuncs.erase(it);
+            
+            for (auto it2 = funcs.begin(); it2 != funcs.end(); ++it2)
+            {
+                if ((&(*it2)) == f)
+                {
+                    funcs.erase(it2);
+                    return;
+                }
             }
         }
         
